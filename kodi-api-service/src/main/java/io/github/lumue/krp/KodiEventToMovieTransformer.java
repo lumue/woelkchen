49d3cbc9adcd8a8606c8aa6a15fc99d6ff2838b0
lm <mueller.lutz@gmail.com>
2018-07-14 13:38:29 +0200

    fix host config

--- kodi-api-service/src/main/java/io/github/lumue/krp/KodiEventToMovieTransformer.java
+++ kodi-api-service/src/main/java/io/github/lumue/krp/KodiEventToMovieTransformer.java
@@ -1,6 +1,8 @@
 package io.github.lumue.krp;
 
 import org.json.JSONObject;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.integration.transformer.Transformer;
 import org.springframework.messaging.Message;
@@ -8,8 +10,11 @@ import org.springframework.messaging.support.MessageBuilder;
 import org.springframework.stereotype.Component;
 import reactor.core.publisher.Mono;
 
+import java.util.logging.FileHandler;
+
 @Component
 public class KodiEventToMovieTransformer implements Transformer {
+	private static final Logger LOGGER = LoggerFactory.getLogger(KodiEventToMovieTransformer.class);
 	private final KodiMovieService kodiMovieService;
 	
 	@Autowired
@@ -19,18 +24,23 @@ public class KodiEventToMovieTransformer implements Transformer {
 	
 	@Override
 	public Message<?> transform(Message<?> message) {
-		
-		JSONObject jsonObject= (JSONObject) message.getPayload();
-		final long movieId = jsonObject.getJSONObject("params")
-				.getJSONObject("data")
-				.getJSONObject("item")
-				.getLong("id");
-		
-		final Movie movie = kodiMovieService.findById(movieId);
-		
-		return MessageBuilder
-				.withPayload(movie)
-				.copyHeadersIfAbsent(message.getHeaders())
-				.build();
+		try {
+			JSONObject jsonObject = (JSONObject) message.getPayload();
+			final long movieId = jsonObject.getJSONObject("params")
+					.getJSONObject("data")
+					.getJSONObject("item")
+					.getLong("id");
+			
+			final Movie movie = kodiMovieService.findById(movieId);
+			
+			return MessageBuilder
+					.withPayload(movie)
+					.copyHeadersIfAbsent(message.getHeaders())
+					.build();
+		}
+		catch (Throwable t){
+			LOGGER.error("error getting movie",t);
+			return null;
+		}
 	}
 }
