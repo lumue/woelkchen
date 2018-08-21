8eb66c67b4e0c86c45961aead93c94bcbd53bc3c
lm <mueller.lutz@gmail.com>
2018-08-21 13:30:40 +0200

    add download-service module

---
+++ download-service/src/main/kotlin/io/github/lumue/mc/downloadservice/DownloadserviceApplication.kt
@@ -0,0 +1,13 @@
+package io.github.lumue.mc.downloadservice
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+import org.springframework.kotlin.experimental.coroutine.EnableCoroutine
+
+@SpringBootApplication
+@EnableCoroutine
+class DownloadserviceApplication
+
+fun main(args: Array<String>) {
+    runApplication<DownloadserviceApplication>(*args)
+}
---
+++ download-service/src/main/kotlin/io/github/lumue/mc/downloadservice/MediaLocation.kt
@@ -0,0 +1,8 @@
+package io.github.lumue.mc.downloadservice
+
+import java.time.LocalDateTime
+
+data class MediaLocation(val url: String,
+                         val added: LocalDateTime) {
+
+}
