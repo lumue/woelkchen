46e694de0b9363bc6e692eb52230659e8bc34ae9
lm <mueller.lutz@gmail.com>
2018-10-22 15:27:24 +0200

    add app-media

---
+++ app-media/src/main/kotlin/io/github/lumue/woelkchen/media/AppMediaApplication.kt
@@ -0,0 +1,11 @@
+package io.github.lumue.woelkchen.media
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class AppMediaApplication
+
+fun main(args: Array<String>) {
+    runApplication<AppMediaApplication>(*args)
+}
