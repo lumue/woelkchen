01a26729d401c473b835e33f2f2333058834aa27
lm <mueller.lutz@gmail.com>
2019-04-19 12:47:52 +0200

    remove unused import

--- app-download/src/main/kotlin/io/github/lumue/woelkchen/download/app/DownloadserviceApplication.kt
+++ app-download/src/main/kotlin/io/github/lumue/woelkchen/download/app/DownloadserviceApplication.kt
@@ -3,7 +3,7 @@ package io.github.lumue.woelkchen.download.app
 
 import org.springframework.boot.SpringApplication
 import org.springframework.boot.autoconfigure.SpringBootApplication
-import org.springframework.boot.runApplication
+
 
 @Suppress("RedundantModalityModifier")
 @SpringBootApplication(scanBasePackages = ["io.github.lumue.woelkchen.download"])
